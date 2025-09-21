ef87d52e7fa011115c0bf60c95944aff7643a030
Dipu-69 <2510610172@geu.ac.in>
2025-09-21 12:59:35 +0000

    2nd

--- backend/routes/testRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-// ✅ Test API route
-router.get("/test", (req, res) => {
-  res.json({ message: "Backend is working!" });
-});
-
-module.exports = router;
